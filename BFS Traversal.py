e1e9cea06feead1b4d1a81d6fcafec77c4a69945
saktidash <sdash2@student.gitam.edu>
2025-03-25 14:36:02 +0530

    Create BFS Traversal.py

---
+++ BFS Traversal.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        if v not in self.graph:
+            self.graph[v] = []
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def bfs(self, start):
+        visited = set()
+        queue = deque([start])
+
+        while queue:
+            node = queue.popleft()
+            if node not in visited:
+                visited.add(node)
+                print(node, end=" ")
+
+                for neighbor in self.graph[node]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+
+if __name__ == "__main__":
+    g = Graph()
+    g.add_edge(1, 2)
+    g.add_edge(1, 3)
+    g.add_edge(2, 4)
+    g.add_edge(2, 5)
+    g.add_edge(3, 6)
+    g.add_edge(5, 6)
+
+    print("BFS Traversal starting from node 1:")
+    g.bfs(1)
