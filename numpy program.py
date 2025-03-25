6c0fa139875d9dfd9ef890954e61e7cf69347806
saktidash <sdash2@student.gitam.edu>
2025-03-25 14:41:54 +0530

    Create numpy program.py

---
+++ numpy program.py
@@ -0,0 +1,9 @@
+# Accessing an element
+print("Element at index 2:", arr1[2])
+
+# Slicing an array
+print("Sliced Array:", arr1[1:4])
+
+# Accessing a submatrix in a 2D array
+print("Submatrix:\n", arr2[0:1, 1:3])
+
